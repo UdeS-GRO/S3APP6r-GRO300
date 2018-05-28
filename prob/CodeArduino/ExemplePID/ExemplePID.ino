1fa2ee4b1baeda8fed2447780329417c5c73f292
François Ferland <git@francoisferland.com>
2018-05-28 21:49:32 +0000

    Cleaned-up ExemplePID (w/o solutions).

--- prob/CodeArduino/ExemplePID/ExemplePID.ino
+++ prob/CodeArduino/ExemplePID/ExemplePID.ino
@@ -1,6 +1,7 @@
 /* Code d'exemple pour controle PID sur Arduino
  * Auteurs: Jean-Samuel Lauzon, 
  *          Jonathan Vincent
+ *          François Ferland
  *          
  * date:    8 avril 2018
 */
@@ -8,77 +9,82 @@
 /* Fonctions de départ pour le shield Arduino_X */ 
 
 
-#include "SimpleTimer.h" // librairie de chronometres
-#include "ArduinoJson.h" // librairie de syntaxe JSON
-#include "Fonctions_GRO.h"
-#include <SPI.h> // librairie Communication SPI
+#include "SimpleTimer.h"    // Librairie de chronomètres
+#include "ArduinoJson.h"    // Librairie de syntaxe JSON
+#include "Fonctions_GRO.h"  // Librairie S3 GRO (capteurs, actionneurs)
+#include <SPI.h>            // librairie Communication SPI
 
 
 /* Constantes */
-#define BAUD            115200 // Frequence de transmission serielle
-#define UPDATE_PERIODE_GEN  100  // Periode (ms) d'envoie d'etat general
+#define BAUD                115200  // Fréquence de transmission sérielle (bauds)
+#define UPDATE_PERIOD_MS    100     // Periode (ms) d'envoie d'etat general
 
+// Les valeurs suivantes correspondent au moteur de test
+// TODO: Assurez-vous d'avoir les bonnes valeurs !
+#define MOTOR_ENC_CPR       300     // Nombre de cycles par tour de l'encodeur
+#define MOTOR_GB_RATIO      100.0   // Ratio de la transmission du moteur (100:1)
+          
 
-Fonctions_GRO fonction; // fonctions supplémentaires
+/* Variables globales */
+Fonctions_GRO s3gro;                // Objet pour l'accès aux actionneurs/capteurs
+
+// Temps (en s) d'un cycle :
+const float DT          = UPDATE_PERIOD_MS / 1000.0;
+// Facteur de conversion encodeur -> tr/s :
+const float ENC_TO_VEL  = 1.0 / (MOTOR_ENC_CPR * MOTOR_GB_RATIO * DT);
 
 // Moteurs
-int pwm1 = 0;
-int pwm2 = 0;
+int motor1_pwm_   = 0;              // Sortie en PWM du moteur 1 (0..255)
+int motor2_pwm_   = 0;              // Sortie en PWM du moteur 2
 
 // Encodeurs
-signed long encoder1count   = 0;
-signed long encoder2count   = 0;  
-signed long last_enc1_count = 0; // Lectures du cycle précédent
-signed long last_enc2_count = 0;
+signed long motor1_enc_count_ = 0;
+signed long motor2_enc_count_ = 0;  
 
-// Electroaimant
-int pinElectroaimant = 32;  // selection Grove possible ... D2, D8, D32
-
-// Lectures Analogique
-int potVex = 0;
-
-// Lecture encodeur Vex
-signed long encodervex = 0;  // selection Grove possible ... D2-D3 interrupt
-
-// Autres variables globales
-float   cur_vel_;           // variable d'état, vitesse du moteur.
-float   des_vel_ = 0.0;     // la consigne en vitesse.
-float   cur_cmd_;           // la commande en cours.
-
-double ENC_TO_VEL = 1000.0/(UPDATE_PERIODE_GEN)/15000;   // Facteur de conversion encodeur -> deg/s.
+// État du moteur à contrôler
+float cur_vel_  = 0.0;              // Vitesse en tr/s.
+float des_vel_  = 0.0;              // Consigne (vitesse désirée) en tr/s.
 
 // Configuration du PID
-float K_P = 0.0; // Facteur P du PID
-float K_I = 0.0; // Facteur I du PID.
-float K_D = 0.0; // Facteur D du PID.
+float K_P = 0.0;                    // Facteur P du PID
+float K_I = 0.0;                    // Facteur I du PID.
+float K_D = 0.0;                    // Facteur D du PID.
 
-// État du PID (inutilisé pour l'instant)
-float errorI = 0;
-float last_error = 0;
+// État du PID (inutilisé pour l'instant, à compléter)
+float P_     = 0;                   // Terme proportionnel
+float I_     = 0;                   // Terme de l'intégrale
+float D_     = 0;                   // Terme de la dérivée 
+float err_I_ = 0;                   // Intégrale de l'erreur
+float min_I_ = -100;                // Valeur minimale de l'intégrale
+float max_I_ =  100;                // Valeur maximale de l'intégrale
+float last_err_ = 0;                // Erreur du cycle précédent
 
-bool    should_send_;       // Indique qu'on est prêt à transmettre l'état.
-SimpleTimer timer;          // objet pour chronometre
-bool state_transmit_; 
-bool state_electroaimant_;   
+// Contrôle du temps
+bool    should_send_;               // Indique qu'on est prêt à transmettre l'état.
+SimpleTimer timer;                  // objet du chronomètre
 
-// Message objects
-StaticJsonBuffer<500> jsonBuffer;
-JsonVariant pi_msg = jsonBuffer.createObject();
-JsonObject& state_msg = jsonBuffer.createObject();
+bool state_transmit_;    
+
+// Gestion du message JSON
+StaticJsonBuffer<500> json_buffer_;
+// Message provenant du Pi :
+JsonVariant           pi_msg_     = json_buffer_.createObject();
+// Message d'état à transmettre au Pi :
+JsonObject&           state_msg_  = json_buffer_.createObject();
 
 
-/* Fonctions et rappels(callbacks) generaux */
+/* Fonctions et rappels(callbacks) */
 
-// Retourne le signe d'un entier (-1 négatif, +1 positif)
+// Retourne le signe d'un entier (-1 négatif, +1 positif, 0 zéro)
 static inline int8_t sgn(int val) {
-  if (val < 0) return -1;
-  if (val==0) return 0;
+  if (val <  0) return -1;
+  if (val == 0) return 0;
   return 1;
 }
 
 void timerCallback(){
   /* Rappel du chronometre */
-  should_send_ = true;
+  should_send_ = true;          // Indique qu'on est prêt à mettre à jour et transmettre
 }
 
 void analyseMessage(JsonVariant msg){
@@ -87,7 +93,7 @@ void analyseMessage(JsonVariant msg){
 
   // Determiner si le message est un succes
   if (!msg.success()) {
-//    Serial.println("parseObject() failed");
+    // Condition d'erreur : On ignore.
     return;
   }
   
@@ -101,18 +107,6 @@ void analyseMessage(JsonVariant msg){
     }
     if (buff_str.equals("false")){
       state_transmit_ = false;
-    }
-  }
-
-  /*Si il y a commande d'Action sur l'aimant */
-  parse_msg =  msg["aimant"];
-  String buff_str1 = msg["aimant"];
-  if(parse_msg.success()){
-    if (buff_str1.equals("true")){
-      state_electroaimant_ = true;
-    }
-    if (buff_str1.equals("false")){
-      state_electroaimant_ = false;
     }
   }
   
@@ -137,24 +131,25 @@ void analyseMessage(JsonVariant msg){
 
 void serialEvent(){
   /* Rappel lors de reception sur port serie */
-  StaticJsonBuffer<500> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(Serial);
-  pi_msg = root;
+  StaticJsonBuffer<500> json_buffer_in;
+  JsonObject& root = json_buffer_in.parseObject(Serial);
+  pi_msg_ = root;
+  analyseMessage(pi_msg_);
+
 }
 
 
-void send_state_data(){
+void sendStateData(){
   /* Fonction pour envoie de l'etat */
-  state_msg.printTo(Serial); // Met le message en attente dans le port serie
+  state_msg_.printTo(Serial); // Met le message en attente dans le port serie
   Serial.println();// Envoie le message
 }
 
-void run_PID(){
+void runPID(){
+  
   float new_cmd = 0.0;
   /* Fonction du controlleur de vitesse */
   
-  /* TODO: Code de contrôle du PID */
-
   /* La sortie du PID doit être inscrite dans la variable "new_cmd".
      Cette variable est ensuite filtrée pour éviter des sauts de commandes lors
      de changements de signes.
@@ -162,59 +157,48 @@ void run_PID(){
      avant de changer de direction.
      C'est le rôle du code qui suit dans la fonction, et vous pouvez le laisser
      intact.
-     Le résultat est stocké dans la variable globale "cur_cmd_".
-     Cette variable contient donc la dernière commande envoyée aux moteurs.
+     
   */
-
-  float err = des_vel_ - cur_vel_;
-  new_cmd = cur_cmd_ + K_P * err;
   
   // Pour s'assurer de passer par zero si changement de signe
-  if (sgn(new_cmd)+sgn(cur_cmd_)==0){
-     fonction.ControleMoteurs(0,0);
+  if (sgn(new_cmd)+sgn(motor1_pwm_)==0){
+     s3gro.ControleMoteurs(0,0);
   }
-
-  cur_cmd_ = new_cmd;
 
   // Si reinitialisation des moteurs
   if (des_vel_ == 0){
-    fonction.ControleMoteurs(0,0);
-    errorI = 0;
-    last_error = 0;
+    motor1_pwm_ = 0;
+    motor2_pwm_ = 0;
+    err_I_ = 0;
+    last_err_ = 0;
   }
   else{
-    // Sinon envoie de la commande au moteur
-    fonction.ControleMoteurs(int(cur_cmd_),0);
+    // Sinon envoie de la commande calculée au moteur 1
+    motor1_pwm_ = (int)new_cmd;
+    motor2_pwm_ = 0;
   }
+
+  s3gro.ControleMoteurs(motor1_pwm_, motor2_pwm_);
 
 }
 
 void updateState(){
 
-    // Lecture Potentiomètre VEX sur A5
-   potVex = analogRead(A5);
-
     // Lecture des pulses d'encodeurs
-    encoder1count = fonction.lireEncodeur(1); 
-    encoder2count = fonction.lireEncodeur(2);
-    cur_vel_ = ENC_TO_VEL*(encoder1count);
+    motor1_enc_count_ = s3gro.lireEncodeur(1); 
+    motor2_enc_count_ = s3gro.lireEncodeur(2);
+    
+    cur_vel_ = ENC_TO_VEL * (motor1_enc_count_);
 
-    fonction.clearEncodeurCount();
-  
-    // Lecture encodeur Vex (D2-D3)
-    encodervex = fonction.lectureEncodeurVex();
-    if(encodervex > 600) fonction.razEncodeurVex(); // exemple de remise à 0
-   // Serial.print(" Encodeur Vex =  "); Serial.println(encodervex); 
+    s3gro.clearEncodeurCount();
  
-    state_msg["enc_1"] = encoder1count;
-    state_msg["enc_2"] = encoder2count;
-    state_msg["pot_vex"] = potVex;
-    state_msg["encodeur_vex"] = encodervex;
-    state_msg["des_vel"] = des_vel_;
-    state_msg["etat_electroaimant"] = state_electroaimant_;
+    state_msg_["enc_1"]   = motor1_enc_count_;
+    state_msg_["enc_2"]   = motor2_enc_count_;
+    state_msg_["des_vel"] = des_vel_;
 
-    // TODO: Variables d'états manquantes: vitesse, commande.
-    send_state_data();
+    // TODO: Variables d'états manquantes: vitesse, commande, PID.
+    
+    sendStateData();
     
     should_send_ = false;
 }
@@ -222,29 +206,29 @@ void updateState(){
 
 void setup()
 {
-    Serial.begin(BAUD); // Initialisation de la communication serielle 
+  Serial.begin(BAUD); // Initialisation de la communication serielle 
   while (!Serial) continue;
 
-   fonction.init();
+  s3gro.init();
 
-    timer.setInterval(UPDATE_PERIODE_GEN, timerCallback);// Initialisation du timer
-    state_transmit_ = false; // met à true pour pas attendre la commande "READ en continue" du PI si non connecté// false;
-    should_send_ == false;
+  timer.setInterval(UPDATE_PERIOD_MS, timerCallback);// Initialisation du timer
+  
+  state_transmit_ = false; // met à true pour pas attendre la commande "READ en continue" du PI si non connecté// false;
+  should_send_    = false;
 
-    delay(1000);
+  delay(1000);
     
 }
 
 void loop() {
 
-/* Fonction de la boucle principale*/
+  /* Fonction de la boucle principale*/
   if(should_send_ == true && state_transmit_ == true){
     updateState();
-    run_PID();
+    runPID();
   }
 
   timer.run();
-  analyseMessage(pi_msg);
 
 }
 
